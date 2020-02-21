132a7fe02eed6defa0a88e8fdc42566944b62f73
tokyorachel <42755840+tokyorachel@users.noreply.github.com>
2020-02-21 16:25:47 +0900

    Adds webpack plugin for moving copying static assets.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
@@ -17,6 +18,12 @@ module.exports = merge(common, {
   },
 
   plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: 'images', // resolves to root/src/images 
+        to: 'images' // copies all files from above dir to dist/images
+      }
+    ]),
     new CleanWebpackPlugin(), // remove dist/ folder of previous build
     new CompressionPlugin() // gzip files
   ]
